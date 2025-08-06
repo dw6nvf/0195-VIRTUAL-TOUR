d8bd98eb37b348e5030df9814691d413fc738865
dw6nvf <oleg.chuprys@gmail.com>
2025-08-06 12:16:54 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-1",
+      "name": "LIVING 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7735724044972052,
+          "pitch": 0.40808074698673913,
+          "rotation": 0,
+          "target": "1-living-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-2",
+      "name": "LIVING 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
